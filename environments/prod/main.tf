1667db39e3fec7710f3936bbeffd38e7a5f29545
Vladimir Romanovskiy <vrmsumy@gmail.com>
2020-11-15 20:35:05 +0000

    Google provider setup

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,23 +1,34 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 locals {
   env = "prod"
 }
 
 provider "google" {
   project = "${var.project}"
+  region  = "${var.region}"
+  zone    = "${var.zone}"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "${var.node_name}"
+  machine_type = "${machine_type}"
+  zone         = "${var.zone}"
+
+  boot_disk {
+    initialize_params {
+      type  = "pd-ssd"
+      image = "ubuntu-cloud/ubuntu-18.04"
+      size  = "100"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata_startup_script = "${file("./start.sh")}"
+}
+
+output "ip" {
+  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
 
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,6 @@
-project="protexc"
+project = "protexc"
+region  = "us-central1"
+zone    = "us-central1-a"
+
+node_name    = "node-x"
+machine_type = "c2-standard-8"
