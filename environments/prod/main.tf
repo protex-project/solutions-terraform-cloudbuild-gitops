cbba94fdda953e59fd5d6d58d9e25e4cf3b70c6c
Vladimir Romanovskiy <vrmsumy@gmail.com>
2020-11-15 20:45:30 +0000

    Google provider setup

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -23,6 +23,9 @@ resource "google_compute_instance" "default" {
 
   network_interface {
     network = "default"
+    access_config {
+	# nat_ip = "${google_compute_address.static.address}"
+    }
   }
 
   metadata_startup_script = "${file("./setup.sh")}"
