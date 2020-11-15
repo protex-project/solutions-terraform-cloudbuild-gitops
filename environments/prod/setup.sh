2c892dc25b52bea293a662a6ecc1efc0d9e2d6d6
Vladimir Romanovskiy <vrmsumy@gmail.com>
2020-11-15 21:19:07 +0000

    Google provider setup

--- environments/prod/setup.sh
+++ environments/prod/setup.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+apt update
+apt install mc
+
+mkdir /usr/local/eosio
+mkdir /usr/local/eosio/bin
+mkdir /usr/local/eosio/etc
+
+cd /root
+cp eosio/bin/cleos /usr/local/eosio/bin
+cp eosio/bin/nodeos /usr/local/eosio/bin
+cp eosio/etc/config.ini /usr/local/eosio/etc
+
+cp backup.sh /root
+cp start_nodeos.sh /root
+
+
