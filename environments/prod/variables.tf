6fc27c648f23f7eaa3c56d721bc8562975eee832
Vladimir Romanovskiy <vrmsumy@gmail.com>
2020-11-16 21:15:50 +0000

    Added variables

--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -20,14 +20,22 @@ variable "producer_name" {
     }
 }
 
-variable "producer_pkey" {
-    type	= string
-    default	= "none"
-    description = "Enter producer PRIVATE key:"
+variable pkeys {
+    description = "Mapping for PRIVATE keys"
+    default = {
+	"inita" = "5KK65pGgYbUzdnWCUQ5FZ6QyhahF3ymtpf5zkv43h7HmLrAmdr5"
+	"initb" = "5J48uZZVdmRxAn8sbosnwG293NnfJ3QG38jGLKJUzfpWajKdYWR"
+	"initc" = "5Khaw3mn1C7xkbRb33qdvADHNkYRaSWrC62tMjdYZGxWczsj6gZ"
+	"initd" = "5Jus9N9Skvc2GL6zaSrfdBHZZwUwiUJmWCpofPgMTwB2Px7ZAPD"
+    }
 }
 
-variable "producer_pub" {
-    type	= string
-    default	= "none"
-    description = "Enter producer PUBLIC key:"
+variable pubs {
+    description = "Mapping for PUBLIC keys"
+    default = {
+	"inita"  = "EOS5zCg5bMvi4AWugeKmiQD5iQtjSrGKdSodTPXnQMYNdeDAm7w2N"
+        "initb" = "EOS6PNy7RbvX1Tj55N7GXFJbwGCmjuDVFDxC224g36MyWTDsUpzbz"
+	"initc"  = "EOS7EXJSLeL3nSmweidjDG9tkeimmYQpt7MCjWiEfP6wDzWYJrSkH"
+	"initd"  = "EOS6W52PxQiFvWPt9DQnK5qdKXdWoJZNWbPdhXE9fBpqx554S2Zre"
+    }
 }
